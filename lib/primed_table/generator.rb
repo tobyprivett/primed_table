60e3aa45199926301ce3737015029a666331ff58
Toby Privett <toby@snaplab.co.uk>
2015-06-25 18:13:05 +0100

    Generate the table with real prime numbers
    - this could be improved by extracting the private
    *_row methods into a Row class and writing robust
    unit tests for those methods

--- lib/primed_table/generator.rb
+++ lib/primed_table/generator.rb
@@ -1,19 +1,45 @@
 module PrimedTable
   class Generator
-    attr_accessor :count
+    attr_accessor :count, :prime_numbers
 
     def initialize(count)
       @count = count
+      @prime_numbers = PrimeNumberBuilder.new(count).list
     end
 
     def draw
-       <<-SAMPLE_TABLE
-        |   2   3   5
-       ----------------
-        2 |   4   6  10
-        3 |   6  10  15
-        5 |  10  15  25
-      SAMPLE_TABLE
+      rows = []
+      rows << header_row
+      rows << seperator_row
+      @prime_numbers.each do |prime_number|
+        rows << computed_row(prime_number)
+      end
+      rows
+    end
+
+    private
+
+    def header_row
+      row_builder(nil, @prime_numbers)
+    end
+
+    def seperator_row
+      row_builder('-', @prime_numbers.map{ |i| '---' })
+    end
+
+    def computed_row(prime_number)
+      row_builder(
+        prime_number,
+        @prime_numbers.map{ |n| n * prime_number }
+      )
+    end
+
+    def row_builder(y_value, numbers)
+      [ pad(y_value), ' |', numbers.map{ |i| pad(i) } ].join
+    end
+
+    def pad(char, length=4)
+      '%4.4s' % char.to_s
     end
   end
 end
--- spec/lib/generator_spec.rb
+++ spec/lib/generator_spec.rb
@@ -2,25 +2,21 @@ require 'spec_helper'
 
 RSpec.describe PrimedTable::Generator do
 
-  let(:sample_table) {
-    <<-SAMPLE_TABLE
-        |   2   3   5
-     ----------------
-      2 |   4   6  10
-      3 |   6  10  15
-      5 |  10  15  25
-    SAMPLE_TABLE
-  }
-
   context '#draw' do
-    let(:primed_table) { described_class.new(count) }
+    let(:primed_table) { described_class.new(3) }
 
-    context 'with 3 prime numbers' do
-      let(:count) { 3 }
+    let(:sample_table) {
+      [
+        "     |   2   3   5",
+        "   - | --- --- ---",
+        "   2 |   4   6  10",
+        "   3 |   6   9  15",
+        "   5 |  10  15  25"
+      ]
+    }
 
-      it 'draws the table' do
-        expect(primed_table.draw).to match(sample_table)
-      end
+    it 'draws the table' do
+      expect(primed_table.draw).to match(sample_table)
     end
   end
 end
